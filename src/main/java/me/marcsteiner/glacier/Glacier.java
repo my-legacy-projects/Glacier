cb3aeed14cc0b2c103d15ebbf7721cf95f7f6968
Marc3842h <git@marcsteiner.me>
2017-10-15 20:32:48 +0200

    Allow configuration of Glacier thought a config.json

--- src/main/java/me/marcsteiner/glacier/Glacier.java
+++ src/main/java/me/marcsteiner/glacier/Glacier.java
@@ -1,14 +1,25 @@
 package me.marcsteiner.glacier;
 
+import com.virtlink.commons.configuration2.jackson.JsonConfiguration;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import me.marcsteiner.glacier.bootstrap.BootstrapOptions;
 import org.apache.commons.cli.*;
+import org.apache.commons.configuration2.builder.DefaultReloadingDetectorFactory;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.kohsuke.MetaInfServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ro.pippo.core.Application;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @MetaInfServices
 public class Glacier extends Application {
@@ -21,6 +32,9 @@ public class Glacier extends Application {
 
     @Getter @Setter(onParam = @__(@NonNull))
     private CommandLine cmdArgs;
+
+    @Getter @Setter(onParam = @__(@NonNull))
+    private JsonConfiguration config;
 
     @Getter @Setter
     private boolean running = false;
@@ -39,6 +53,40 @@ public class Glacier extends Application {
             formatter.printHelp("argument", options);
             System.exit(-1);
         }
+
+        String confPath = getInstance().getCmdArgs().getOptionValue("config");
+        if(confPath == null) {
+            confPath = "config.json";
+        }
+        if(!confPath.endsWith(".json")) {
+            confPath += ".json";
+        }
+
+        File confFile = new File(Paths.get(".").toAbsolutePath().normalize().toFile(), confPath);
+        if(!confFile.exists()) {
+            try {
+                Files.copy(Glacier.class.getResourceAsStream("/config.json"), confFile.toPath());
+            } catch (IOException ex) {
+                getInstance().getLogger().error("Could not create default config.json file.", ex);
+                System.exit(-1);
+            }
+        }
+
+        try {
+            getInstance().setConfig(new FileBasedConfigurationBuilder<>(JsonConfiguration.class)
+                    .configure(new Parameters().properties()
+                            .setFileName(confPath)
+                            .setThrowExceptionOnMissing(true)
+                            .setListDelimiterHandler(new DefaultListDelimiterHandler(';'))
+                            .setReloadingDetectorFactory(new DefaultReloadingDetectorFactory())
+                            .setEncoding("UTF-8"))
+            .getConfiguration());
+        } catch (ConfigurationException ex) {
+            getInstance().getLogger().error("Could not load config.json file. " +
+                    "Please run it the config.json file thought a JSON validator and check for missing keys.", ex);
+            System.exit(-1);
+        }
+
 
     }
 
